0f4092a424005e2715fdcdfd1598ea375d7b378c
StickonFire <70039286+StickonFire@users.noreply.github.com>
2022-02-04 13:32:40 -0800

    Ready for FailCase

--- ARemoval.java
+++ ARemoval.java
@@ -1,2 +1,9 @@
 public class ARemoval{
+    public static String removeA(String remove){
+        while(remove.indexOf("a") != -1){
+            int index = remove.indexOf("a");
+            remove = (remove.substring(0,index) + remove.substring(index+1));
+        }
+        return remove;
+    }
 }
---
+++ ARemovalTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class ARemovalTest {
+    @Test
+    public void testAnna(){
+        assertEquals(ARemoval.removeA("Anna"),"nn");
+    }
+}
